a5662a95e56d52df2debcc4d798a1647aaa439e4
Mark Fee <git@markfee.com>
2015-11-08 08:48:46 +0000

    Added a new test to the HelloWorldSpec

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,1 +1,3 @@
-object HelloWorld { def main(args: Array[String]) = println("Hello World!") }
+object HelloWorld {
+  def main(args: Array[String]) = println("Hello world")
+}
--- src/test/scala/HelloWorldSpec.scala
+++ src/test/scala/HelloWorldSpec.scala
@@ -1,8 +1,19 @@
-import org.specs2_
+import org.specs2.Specification
+
 class HelloWorldSpec extends Specification { def is = s2"""
 
   This is my first specification
     it is working                 $ok
     really working!               $ok
-                                  """
+
+  This is a specification for the 'Hello world' string
+
+  The 'Hello world' string should
+    contain 11 characters                             $e1
+    start with 'Hello'                                $e2
+    end with 'world'                                  $e3
+  """
+  def e1 = "Hello world" must haveSize(11)
+  def e2 = "Hello world" must startWith("Hello")
+  def e3 = "Hello world" must endWith("world")
 }
