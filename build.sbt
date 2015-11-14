2fdd566224f0c22a36acfb6cc2d8266bc39b35f8
Mark Fee <git@markfee.com>
2015-11-14 09:03:39 +0000

    refactored AnagramFinderSpec to use mutableSpecification with Mockito

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "1.0"
 scalaVersion := "2.10.5"
 
 libraryDependencies ++= Seq("org.specs2" %% "specs2-core" % "3.6.5" % "test")
+libraryDependencies ++= Seq("org.specs2" %% "specs2-mock" % "3.6.5" % "test")
 
 scalacOptions in Test ++= Seq("-Yrangepos")
 
--- src/test/scala/AnagramFinderSpec.scala
+++ src/test/scala/AnagramFinderSpec.scala
@@ -1,18 +1,15 @@
-import org.specs2.Specification
+import org.specs2.mock._
+import org.specs2.mutable.Specification
 
-class AnagramFinderSpec extends Specification { def is = s2"""
-  I can get an instance of an AnagramFinder
-    valid instance          $anagramFinder1
-    should contain the word that we are looking for anagrams of $hasword
-
-  I can check that a word is entirely contained within another word
-    hell is contained in hello $hello_contains_hell
-    help is not contained in hello  $hello_does_not_contains_help
-    leo is contained in hello  $hello_contains_leo
-  """
-  def anagramFinder1 = new AnagramFinder("hello") must beAnInstanceOf[AnagramFinder]
-  def hasword = new AnagramFinder("hello").word must be_==("hello")
-  def hello_contains_hell = new AnagramFinder("hello").contains("hell") must be_==(true)
-  def hello_does_not_contains_help = new AnagramFinder("hello").contains("help") must be_==(false)
-  def hello_contains_leo = new AnagramFinder("hello").contains("leo") must be_==(true)
+class AnagramFinderSpec extends Specification with Mockito {
+  "AnagramFinder" should {
+    "be instantiable" in {
+      new AnagramFinder("hello") must beAnInstanceOf[AnagramFinder]
+    }
+    "I can check that a word is entirely contained within another word" in {
+      new AnagramFinder("hello").contains("hell") must be_==(true)
+      new AnagramFinder("hello").contains("help") must be_==(false)
+      new AnagramFinder("hello").contains("leo") must be_==(true)
+    }
+  }
 }
